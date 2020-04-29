c71b5a080feb7b66b891faf01b1a2931910e0d17
Vesa Qilerxhiu <44980967+vesaqilerxhiu@users.noreply.github.com>
2020-04-30 00:02:17 +0200

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	$host='localhost';
+	$username='root';
+	$pass='Vesa1234';
+	$db='projektiphp';
+
+	$conn=mysqli_connect($host,$username,$pass,$db);
+
+	if(!$conn) die("Connection refused").mysql_connect_error();
+
+ ?>
