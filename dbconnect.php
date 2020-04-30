715185f9627c457fbf0ff964c7de32b03fe8ad3e
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-04-30 13:15:58 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -5,9 +5,8 @@
 	$pass='Vesa1234';
 	$db='projektiphp';
     //konektimi
-//lidhja me db
+    //lidhja me db
 	$conn=mysqli_connect($host,$username,$pass,$db);
-
 	if(!$conn) die("Connection refused").mysql_connect_error();
 
  ?>
