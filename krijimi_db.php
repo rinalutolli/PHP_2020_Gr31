3661bc5ffa646b10d8d303e27ff4bf849e821bc6
Elda <44511801+eldalamaxhema@users.noreply.github.com>
2020-04-30 23:39:39 +0200

    Add files via upload

---
+++ krijimi_db.php
@@ -0,0 +1,29 @@
+<?php
+
+require("dbconfig.php");
+
+
+
+
+//MySQLi Procedurale
+$conn = mysqli_connect(servername, username, password); 
+if (!$conn) {  
+   die("Connection failed: " . mysqli_error($conn)); 
+}
+
+$sql = 'CREATE Database ushtrime';
+
+
+$retval = mysqli_query( $conn, $sql );
+if(! $retval )
+{
+die('Could not create database: ' . mysqli_error($conn));
+}
+echo "Database Ushtrime created successfully\n";
+mysqli_close($conn);
+	
+
+
+
+
+?>
