1be00284377e5bdeedfafa0edd8cde6434f3a59d
rinalutolli <52406042+rinalutolli@users.noreply.github.com>
2020-05-02 23:50:11 +0200

    Add files via upload

--- krijimi_tabelaveR.php
+++ krijimi_tabelaveR.php
@@ -13,7 +13,7 @@ if (!$conn) {
 //mysqli_select_db($conn, dbname);
 
 $sql = "CREATE TABLE users (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id INT(6) UNSIGNED AUTO_INCREMENT,
 name VARCHAR(30) NOT NULL,
 surname VARCHAR(30) NOT NULL,
 birthday DATE,
@@ -23,6 +23,13 @@ password VARCHAR(50),
 c_password VARCHAR(50),
 data_regjistrimit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
+$sql = "CREATE TABLE login (
+lid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+lemail VARCHAR(50),
+lpassword VARCHAR(50),
+data_loginit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
  
 $retval = mysqli_query($conn, $sql );
 if(! $retval )
