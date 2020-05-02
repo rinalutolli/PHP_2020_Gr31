80a5ddb32355208f7af9eb2da62654f7f2a997af
Elda <44511801+eldalamaxhema@users.noreply.github.com>
2020-05-02 23:50:42 +0200

    Add files via upload

---
+++ krijimi_tabelaveE.php
@@ -0,0 +1,72 @@
+<?php
+
+require("dbconfig.php");
+
+
+//MySQLi Procedurale
+$conn = mysqli_connect(servername, username, password, dbname); 
+if (!$conn) {  
+   die("Connection failed: " . mysqli_connect_error()); 
+}
+
+//OSE
+//mysqli_select_db($conn, dbname);
+
+
+
+
+
+
+
+
+
+
+
+$sql = "CREATE TABLE users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+surname VARCHAR(30) NOT NULL,
+birthday DATE,
+gender VARCHAR(30),
+email VARCHAR(30),
+password VARCHAR(30),
+c_password VARCHAR(30)
+)";
+
+
+
+
+
+
+
+
+$sql = "CREATE TABLE login (
+lid INT(6) UNSIGNED AUTO_INCREMENT ,
+lemail VARCHAR(50)  PRIMARY KEY,
+lpassword VARCHAR(50),
+data_loginit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+
+
+ 
+$retval = mysqli_query($conn, $sql );
+if(! $retval )
+{
+die('Nuk mund te krijohet tabla: ' . mysqli_error($conn));
+}
+echo "Tabela u krijua me sukses\n";
+mysqli_close($conn);
+
+
+
+
+
+
+
+
+
+
+	
+?>
+
