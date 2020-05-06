f980d7a78a5d7ef45575aa5e1dd7a046f147a128
Elda <44511801+eldalamaxhema@users.noreply.github.com>
2020-05-07 00:47:42 +0200

    Add files via upload

--- loginE.php
+++ loginE.php
@@ -1,3 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = $_POST['lemail'];
+setcookie("lemail", $email , time() + (86400 * 30), "/");
+setcookie("lemail", "", time() - 3600);
+}
+?>
+<?php
+session_start();
+$message="";
+if(count($_POST)>0) {
+ $con = mysqli_connect('127.0.0.1:3306','elda','12345','ushtrimet') or die('Unable To connect');
+$result = mysqli_query($con,"SELECT * FROM login WHERE lemail='" . $_POST["lemail"] . "' and lpassword = '". $_POST["lpassword"]."'");
+$row  = mysqli_fetch_array($result);
+if(is_array($row)) {
+$_SESSION["lemail"] = $row['lemail'];
+$_SESSION["lpassword"] = $row['lpassword'];
+} else {
+$message = "Invalid Username or Password!";
+}
+}
+if(isset($_SESSION["lemail"])) {
+header("Location:index.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,20 +33,20 @@
   $(document).ready(function(){
    $("input").focus(function(){
     $(this).css("background-color", "yellow");
-  
+ 
   });
   $("input").blur(function(){
     $(this).css("background-color","lightgreen");
   });
 });
 </script>
-
-
+ 
+ 
   <style type="text/css">
   .grad {
   background-image: radial-gradient(white, lightgrey, grey);
 }
-
+ 
      body
   {
     background-color: lightgray;
@@ -42,9 +67,9 @@
       padding:0 0.5em 0 0.5em;
       border:groove gray;
       border-radius: 10px;
-      
+     
       text-align: center;
-
+ 
     }
      #rv{
       background-color: black;
@@ -56,7 +81,7 @@
     {
       text-decoration: none;
       color:grey;
-
+ 
     }
     a:hover
     {
@@ -64,7 +89,7 @@
       border-radius: 10px;
       padding:0.5em;
     }
-    
+   
     input
     {
       padding: 0.5em;
@@ -78,7 +103,7 @@
       padding: 1em;
       border-radius: 10px;
     }
-     button a 
+     button a
     {
       text-decoration: none
       font-family:sans-serif;
@@ -90,38 +115,38 @@
       color: white;
       padding: 1em;
       border-radius: 10px;
-
+ 
     }
     </style>
-
+ 
  
    
 </head>
 <body >
-
+ 
   <div class="grad">
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"> 
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+   
     <h4 id="rv">Sign In to our community</h4>
     <h3>Email address</h3>
     <input type="text" name="lemail" placeholder="Enter your email address" autocomplete="on">
-    <h5 style="color:red">
-<?php
-
+    <h5 style="color:red"><?php
+ 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST['lemail'];
   if (empty($email)) {
-      echo "Email is empty!<br>";
+      echo "Email is empty!";
   }
    else if (!preg_match('/[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z0-9\.\-]+$/',$email)) {
-    echo "This is an invalid email.<br>";
+    echo "This is an invalid email.";
   }
 }
-
+ 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
   $email = $_POST['lemail'];
   $vargu = explode("@",$email);
  
-  
+ 
   if (empty($vargu[0])) {
       echo "There is no domain and username.";}
   elseif (empty($vargu[1])){
@@ -130,57 +155,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
   else {
     echo "Username: ". $vargu[0]. "<br/>";
     echo "Domain: ". $vargu[1]."<br/>";}
-    
+   
    }
-
+ 
 ?></h5>
     <h3>Password</h3>
     <input type="password" name="lpassword" keygen="password" placeholder="Enter your password"><br/><br/>
-    <input type="submit" name="add" id="add" value="Sign  in">
-
+    <input type="submit" name="add" id="add" value="Sign in">
+ 
     <p id="fp"><a href="#">Forgot your password?</a></p>
-    
+   
   </div>
-
+ 
   <div id="cr" class="grad">
     <h3>New to Mag Magazine?</h3>
     <h3><a href="signup.php">Sign Up</a></h38>
   </form>  
-
+ 
   </div>
   <?php
-
+ 
 require("dbconfig.php");
-
+ 
 $conn = mysqli_connect(servername, username, password, dbname);
 if (!$conn) {
 die("Connection failed: " . mysqli_connect_error());
 }
-
+ 
 if(isset($_POST['add'])){
-
-
+ 
+ 
 $lemail = $_POST['lemail'];
 $lpassword=$_POST['lpassword'];
-
-
+ 
+ 
 $sql = "INSERT INTO login (lemail, lpassword)
 VALUES ('$lemail','$lpassword')";
-
+ 
 $retval = mysqli_query($conn, $sql );
 if(! $retval )
 {
 die('Nuk mund te shtohen te dhenat ne tabele: ' . mysqli_error($conn));
 }
-
+ 
 $last_id = mysqli_insert_id($conn); //ID e rekordit te fundit
 echo "Te dhenat u shtuan me sukses.\nID eshte: ". $last_id;
 mysqli_close($conn);
 }
-
+ 
 ?>
-
-    
-
+ 
+   
+ 
 </body>
 </html>
