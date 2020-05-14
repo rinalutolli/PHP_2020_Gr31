1c98d7962d615b34fed531492f5e13339bee4cd0
Elda <44511801+eldalamaxhema@users.noreply.github.com>
2020-05-14 23:08:35 +0200

    Add files via upload

---
+++ deletee.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "connection.php";
+ 
+	$sql = "DELETE FROM users WHERE name='" . $_GET["name"] . "' ";
+	$rezultati = mysqli_query($conn, $sql);
+	if(! $rezultati )
+	{
+	die('Data cannot be deleted: ' . mysqli_error($conn));
+	}
+	else
+	{
+		
+		header("Location:members.php");
+	}
+?>
