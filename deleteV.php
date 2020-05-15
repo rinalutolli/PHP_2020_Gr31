967794d736a7c8bde574638183894aedfa41d51d
Vesa Qilerxhiu <44980967+vesaqilerxhiu@users.noreply.github.com>
2020-05-15 23:09:46 +0200

    Add files via upload

--- deleteV.php
+++ deleteV.php
@@ -2,7 +2,7 @@
 
 require_once "connection.php";
  
-	$sql = "DELETE FROM users WHERE name='" . $_GET["name"] . "' ";
+	$sql = "DELETE FROM users WHERE id='" . $_GET["id"] . "' ";
 	$rezultati = mysqli_query($conn, $sql);
 	if(! $rezultati )
 	{
