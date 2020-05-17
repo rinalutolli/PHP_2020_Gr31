1d2f80f65d75210205ee0e19b78b4ae3bb136502
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-05-17 18:12:58 +0200

    Update sportF.php

--- sportF.php
+++ sportF.php
@@ -9,9 +9,6 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="jquery-1.11.2.min.js"></script>
 <script
-        
-      
-        
         >
 $(document).ready(function(){
   $("#button2").click(function(){
